54e43cf52abdd412335654c996236fb1696cc799
niranjanravi1993 <ravin@iu.edu>
2023-08-18 15:22:18 -0400

    uploading iiou for 3D boxes

--- networktrain.sh
+++ networktrain.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=ravin@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
-#SBATCH --time=0-1:0:00
+#SBATCH --time=0-6:0:00
 #SBATCH --mem=64gb
 #SBATCH --partition=gpu
 #SBATCH --gpus v100:1
@@ -20,20 +20,3 @@ module load cudatoolkit/11.2
 cd /N/slate/ravin/Fall2024/Improved_3D_IOU/tools
 
 python train.py --cfg_file ./cfgs/kitti_models/rdiou.yaml
-
-
-#CUDA_VISIBLE_DEVICES=0,1 bash scripts/dist_train.sh 2 --cfg_file ./cfgs/kitti_models/biprodet.yaml
-
-#python test.py --eval_all --cfg_file ./cfgs/kitti_models/biprodet.yaml
-
-#python setup.py develop
-
-#python -m pcdet.datasets.kitti.kitti_dataset create_kitti_infos tools/cfgs/dataset_configs/kitti_dataset.yaml
-
-#python train.py --cfg_file ./cfgs/kitti_models/biprodet.yaml
-
-#CUDA_VISIBLE_DEVICES=0,1 bash scripts/dist_train.sh 4 --cfg_file ./cfgs/kitti_models/biprodet.ya
-
-#sh dist_train.sh
-
-
--- pcdet/models/dense_heads/anchor_head_rdiou.py
+++ pcdet/models/dense_heads/anchor_head_rdiou.py
@@ -6,9 +6,28 @@ from ...utils import loss_utils
 from .anchor_head_template import AnchorHeadTemplate
 from .target_assigner.atss_target_assigner import ATSSTargetAssigner
 from .target_assigner.axis_aligned_target_assigner_add_gt import AxisAlignedTargetAssigner
-
+import logging
 import pdb
 
+log_file = ('iiou_from_rdiou.txt')
+
+def create_logger_loss(log_file=None, rank=0, log_level=logging.INFO):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level if rank == 0 else 'ERROR')
+    formatter = logging.Formatter('%(asctime)s  %(levelname)5s  %(message)s')
+    console = logging.StreamHandler()
+    console.setLevel(log_level if rank == 0 else 'ERROR')
+    console.setFormatter(formatter)
+    logger.addHandler(console)
+    if log_file is not None:
+        file_handler = logging.FileHandler(filename=log_file)
+        file_handler.setLevel(log_level if rank == 0 else 'ERROR')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    logger.propagate = False
+    return logger
+    
+logger_iou = create_logger_loss(log_file)
 
 class AnchorHeadRDIoU(AnchorHeadTemplate):
     def __init__(self, model_cfg, input_channels, num_class, class_names, grid_size, point_cloud_range,
@@ -74,7 +93,7 @@ class AnchorHeadRDIoU(AnchorHeadTemplate):
             loss_utils.WeightedCrossEntropyLoss()
         )
     
-    def get_rdiou(self, bboxes1, bboxes2):
+    def get_rdiou_old(self, bboxes1, bboxes2):
         x1u, y1u, z1u = bboxes1[:,:,0], bboxes1[:,:,1], bboxes1[:,:,2]
         l1, w1, h1 =  torch.exp(bboxes1[:,:,3]), torch.exp(bboxes1[:,:,4]), torch.exp(bboxes1[:,:,5])
         t1 = torch.sin(bboxes1[:,:,6]) * torch.cos(bboxes2[:,:,6])
@@ -125,6 +144,90 @@ class AnchorHeadRDIoU(AnchorHeadTemplate):
         u = (inter_diag) / c_diag
         rdiou = inter_volume / union
         return u, rdiou
+        
+    def get_rdiou(self, bboxes1, bboxes2):  #NiranjanCode
+		
+        x1u, y1u, z1u = bboxes1[:,:,0], bboxes1[:,:,1], bboxes1[:,:,2]
+        l1, w1, h1 =  torch.exp(bboxes1[:,:,3]), torch.exp(bboxes1[:,:,4]), torch.exp(bboxes1[:,:,5])
+        t1 = torch.sin(bboxes1[:,:,6]) * torch.cos(bboxes2[:,:,6])
+        x2u, y2u, z2u = bboxes2[:,:,0], bboxes2[:,:,1], bboxes2[:,:,2]
+        l2, w2, h2 =  torch.exp(bboxes2[:,:,3]), torch.exp(bboxes2[:,:,4]), torch.exp(bboxes2[:,:,5])
+        t2 = torch.cos(bboxes1[:,:,6]) * torch.sin(bboxes2[:,:,6]) 
+        #logger_iou.info("iiou code is executing")
+
+        # we emperically scale the y/z to make their predictions more sensitive.
+        x1 = x1u
+        y1 = y1u * 2
+        z1 = z1u * 2
+        x2 = x2u
+        y2 = y2u * 2
+        z2 = z2u * 2
+
+        # clamp is necessray to aviod inf.
+        l1, w1, h1 = torch.clamp(l1, max=10), torch.clamp(w1, max=10), torch.clamp(h1, max=10)
+        j1, j2 = torch.ones_like(h2), torch.ones_like(h2)
+
+        volume_1 = l1 * w1 * h1 * j1
+        volume_2 = l2 * w2 * h2 * j2
+
+        inter_l = torch.max(x1 - l1 / 2, x2 - l2 / 2)
+        inter_r = torch.min(x1 + l1 / 2, x2 + l2 / 2)
+        inter_t = torch.max(y1 - w1 / 2, y2 - w2 / 2)
+        inter_b = torch.min(y1 + w1 / 2, y2 + w2 / 2)
+        inter_u = torch.max(z1 - h1 / 2, z2 - h2 / 2)
+        inter_d = torch.min(z1 + h1 / 2, z2 + h2 / 2)
+        inter_m = torch.max(t1 - j1 / 2, t2 - j2 / 2)
+        inter_n = torch.min(t1 + j1 / 2, t2 + j2 / 2)
+
+        inter_volume = torch.clamp((inter_r - inter_l),min=0) * torch.clamp((inter_b - inter_t),min=0) \
+            * torch.clamp((inter_d - inter_u),min=0) * torch.clamp((inter_n - inter_m),min=0)
+        
+        c_l = torch.min(x1 - l1 / 2,x2 - l2 / 2)
+        c_r = torch.max(x1 + l1 / 2,x2 + l2 / 2)
+        c_t = torch.min(y1 - w1 / 2,y2 - w2 / 2)
+        c_b = torch.max(y1 + w1 / 2,y2 + w2 / 2)
+        c_u = torch.min(z1 - h1 / 2,z2 - h2 / 2)
+        c_d = torch.max(z1 + h1 / 2,z2 + h2 / 2)
+        c_m = torch.min(t1 - j1 / 2,t2 - j2 / 2)
+        c_n = torch.max(t1 + j1 / 2,t2 + j2 / 2)
+
+        inter_diag = (x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2 + (t2 - t1)**2
+        c_diag = torch.clamp((c_r - c_l),min=0)**2 + torch.clamp((c_b - c_t),min=0)**2 + torch.clamp((c_d - c_u),min=0)**2  + torch.clamp((c_n - c_m),min=0)**2
+
+        union = volume_1 + volume_2 - inter_volume
+        u = (inter_diag) / c_diag
+        rdiou = inter_volume / union
+        
+        #Niranjan Code
+        
+        len_factor = l1/l2
+        wid_factor = w1/w2
+        ht_factor  = h1/h2
+        
+        x1_min = x1 - l1
+        x1_max = x1 + l1
+        y1_min = y1 - w1
+        y1_max = y1 + w1
+        z1_min = z1 - h1
+        z1_max = z1 + h1
+        
+        x2_min = x2 - l2
+        x2_max = x2 + l2
+        y2_min = y2 - w2
+        y2_max = y2 + w2
+        z2_min = z2 - h2
+        z2_max = z2 + h2
+        
+        eps=1e-7
+        
+        distance_between_min_edges = (x1_min - x2_min) ** 2+ (y1_min - y2_min) ** 2 + (z1_min - z2_min) ** 2 + eps
+        
+        distance_between_max_edges = (x1_max - x2_max) ** 2+ (y1_max - y2_max) ** 2 + (z1_max - z2_max) ** 2 + eps
+        
+        eucledian_part = distance_between_min_edges + distance_between_max_edges
+        iiou = eucledian_part/c_diag
+          
+        return u, iiou
 
     def get_clsreg_targets(self):
         box_preds = self.forward_ret_dict['box_preds']
